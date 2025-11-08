eed66ad98780e766eacc4969dbc5c9aaf2df4ed8
ManuelGraziosi <manuel.graziosi@gmail.com>
2025-11-08 12:05:21 +0100

    feat: list of element implemented (Milestone 1)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import { useState } from 'react'
 import './App.css'
+import blogPosts from './assets/listOfPost'
 
 function App() {
 
   return (
     <>
+      <h1>React Form</h1>
+      <div className="container">
+        <h2>Elenco Articoli:</h2>
+        <ul className="list-group">
+          {
+            blogPosts.map((curBlog) => (
+              <li key={curBlog.id} className="list-group-item">{curBlog.titolo}</li>
+            ))
+          }
+
+        </ul>
+      </div>
     </>
   )
 }
